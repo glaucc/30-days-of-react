1d933559261752b88a5032f5b7f82567f1c172ca
glaucc <ahmedovyusif8785@gmail.com>
2023-05-11 06:27:05 +0400

    debugging

--- props/src/components/ContantForm.jsx
+++ props/src/components/ContantForm.jsx
@@ -1,18 +1,52 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
+import nodemailer from 'nodemailer';
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+
 
 function ContactForm() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
 
+  // const nodemailer = require('nodemailer');
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+  
     const data = { name, email, message };
+    const EMAIL = "";
+    const PASSWORD = "";
+
+    const nodemailer = require('nodemailer');
 
     try {
-      await axios.post('/sendEmail', data);
+      const transporter = nodemailer.createTransport({
+        host: 'smtp.example.com',
+        port: 587,
+        secure: false,
+        service: 'gmail',
+        auth: {
+          user: EMAIL, // Replace with your Gmail email address
+          pass: PASSWORD // Replace with your Gmail password
+        }
+      });
+  
+      const mailOptions = {
+        from: EMAIL, // Replace with your Gmail email address
+        to: EMAIL, // Replace with the recipient email address
+        subject: 'New message from your website',
+        html: `
+          <p><strong>Name:</strong> ${name}</p>
+          <p><strong>Email:</strong> ${email}</p>
+          <p><strong>Message:</strong> ${message}</p>
+        `
+      };
+  
+      await transporter.sendMail(mailOptions);
+  
       alert('Email sent successfully!');
     } catch (error) {
       console.error(error);
@@ -21,7 +55,8 @@ function ContactForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    
+    <form onSubmit={handleSubmit} className='mainForm'>
       <div>
         <label htmlFor="name">Name:</label>
         <input
@@ -42,7 +77,7 @@ function ContactForm() {
           required
         />
       </div>
-      <div>
+      <div className='messageBlock'>
         <label htmlFor="message">Message:</label>
         <textarea
           id="message"
